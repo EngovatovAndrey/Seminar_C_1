6b7808766d3d881628353aa23e609e336b4df89b
Andrey Engo <5522662@gmail.com>
2022-12-15 21:46:00 +0300

    add Project004 and Project005

---
+++ Seminar_C_1/Project004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+using static System.Console;
+
+WriteLine("Введите число -");
+int n = int.Parse(ReadLine()!);
+int count = -n;
+
+while (count != n+1){
+    WriteLine(count); 
+    count++;
+}
+
+
